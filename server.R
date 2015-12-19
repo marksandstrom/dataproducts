3bb612c83d3dad167100b43a5b89cd3014f4c981
marksandstrom <marksandstrom@users.noreply.github.com>
2015-12-19 17:39:44 +0200

    Update server.R

--- server.R
+++ server.R
@@ -7,23 +7,23 @@ L = length(s)
 P = sum(l==1)
 N = sum(l==0)
 
-tpr=tnr=rep(0,100)
+tpr = tnr = rep(0,100)
 for (i in 1:100) {
-  cutoff=i/100
-  sl=as.numeric(s>cutoff)
-  c=sl==l
-  tpr[i]=sum(c&(l==1))/P
-  tnr[i]=sum(c&(l==0))/N
+  cutoff = i/100
+  sl = as.numeric(s>cutoff)
+  c = sl==l
+  tpr[i] = sum(c&(l==1))/P
+  tnr[i] = sum(c&(l==0))/N
 }
 
 shinyServer(
   function(input, output) {
-    output$newPlot=renderPlot({
-      a=input$a
-      b=1-a
-      score=a*tpr+b*tnr
+    output$newPlot = renderPlot({
+      a = input$a
+      b = 1-a
+      score = a*tpr+b*tnr
       plot(1:100/100,score,col='blue',xlab="cutoff",ylab="Weighted sum of TPR and NPR")
-      co=input$co
+      co = input$co
       lines(c(co,co), c(0,2), col='red', lwd=3)
       text(.5,.65,paste("Score=",round(score[co*100],2)),col='blue')
       text(.5,.6,paste("cutoff=",co),col='red')
