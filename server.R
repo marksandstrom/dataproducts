be5bad84832a74b250fca2367cf456e978c7587e
marksandstrom <marksandstrom@users.noreply.github.com>
2015-12-19 17:36:59 +0200

    Update server.R

--- server.R
+++ server.R
@@ -1,16 +1,33 @@
-library(UsingR)
-data(galton)
+library(ROCR)
+data(ROCR.simple)
+dr = as.data.frame(ROCR.simple)
+s = dr$predictions + rnorm(200)*.1 - 200:1/2000
+l = dr$labels
+L = length(s)
+P = sum(l==1)
+N = sum(l==0)
+
+tpr=tnr=rep(0,100)
+for (i in 1:100) {
+  cutoff=i/100
+  sl=as.numeric(s>cutoff)
+  c=sl==l
+  tpr[i]=sum(c&(l==1))/P
+  tnr[i]=sum(c&(l==0))/N
+}
 
 shinyServer(
   function(input, output) {
-    output$newHist <- renderPlot({
-      hist(galton$child, xlab='child height', col='lightblue',main='Histogram')
-      mu <- input$mu
-      lines(c(mu, mu), c(0, 200),col="red",lwd=5)
-      mse <- mean((galton$child - mu)^2)
-      text(63, 150, paste("mu = ", mu))
-      text(63, 140, paste("MSE = ", round(mse, 2)))
-    })
-    
+    output$newPlot=renderPlot({
+      a=input$a
+      b=1-a
+      score=a*tpr+b*tnr
+      plot(1:100/100,score,col='blue',xlab="cutoff",ylab="Weighted sum of TPR and NPR")
+      co=input$co
+      lines(c(co,co), c(0,2), col='red', lwd=3)
+      text(.5,.65,paste("Score=",round(score[co*100],2)),col='blue')
+      text(.5,.6,paste("cutoff=",co),col='red')
+    })    
   }
 )
+ 
