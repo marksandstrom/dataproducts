fbb5cddcc045699fa2026e72d51113e22a2c68aa
marksandstrom <marksandstrom@users.noreply.github.com>
2015-12-21 20:54:39 +0200

    Update server.R

--- server.R
+++ server.R
@@ -1,9 +1,9 @@
 library(ROCR)
 data(ROCR.simple)
 dr = as.data.frame(ROCR.simple)
-s = dr$predictions + rnorm(200)*.1 - 200:1/2000
 l = dr$labels
-L = length(s)
+L = length(l)
+s = dr$predictions + rnorm(L)*.1 - L:1/(L*10)
 P = sum(l==1)
 N = sum(l==0)
 
@@ -22,12 +22,12 @@ shinyServer(
       a = input$a
       b = 1-a
       score = a*tpr+b*tnr
-      plot(1:100/100,score,col='blue',xlab="cutoff",ylab="Weighted sum of TPR and NPR")
+      plot(1:100/100,score,col='blue',xlab='cutoff',ylab=paste('SCORE:',a,'* TPR + ',b,'* TNR'))
       co = input$co
       lines(c(co,co), c(0,2), col='red', lwd=3)
-      text(.5,.65,paste("Score=",round(score[co*100],2)),col='blue')
-      text(.5,.6,paste("cutoff=",co),col='red')
-    })    
+      x = ifelse (co<.5,co+.1,co-.1)
+      text(x,.65,paste("SCORE =",round(score[co*100],2)),col='blue')
+      text(x,.6,paste("cutoff =",co),col='red')    })    
   }
 )
  
