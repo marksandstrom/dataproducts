f3358e39ce16dac342610ab151f60b5252dc89db
marksandstrom <marksandstrom@users.noreply.github.com>
2015-12-21 20:55:30 +0200

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,7 +1,7 @@
 shinyUI(pageWithSidebar(
-  headerPanel("Find classification cutoff for maxizing weighted sum of True Positive Rate (TPR) and True Negative Rate (TNR)"),
+  headerPanel("Find classification cutoff for maximizing weighted sum of True Positive Rate (TPR) and True Negative Rate (TNR)"),
   sidebarPanel(
-    sliderInput('a', 'Enter weight for TPR (weight for NPR = 1-weight for TPR)', .5, min = 0, max = 1, step = .01),
+    sliderInput('a', 'Enter weight for TPR (weight for TNR = 1-weight for TPR)', .5, min = 0, max = 1, step = .01),
     sliderInput('co', 'Enter desired cutoff', .5, min = 0, max = 1, step = .01)    
   ),
   mainPanel(
